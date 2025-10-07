0abb9c2a849bab37173f60e8e523346b95bf3075
Mostakin chowdhury <chowdhurymostakin02@gmail.com>
2025-10-08 03:18:46 +0600

    solution

--- js.js
+++ js.js
@@ -1,13 +1,8 @@
 const name = document.getElementById('name'),
   head = document.querySelector('header'),
   ctrl = document.querySelector('.ctrl'),
-  progressContainer = document.getElementById('progresscontainer'),
-  loader = document.getElementById('loader')
+  progressContainer = document.getElementById('progresscontainer')
 
-window.onload = () => {
-  loader.classList.remove('fixed')
-  loader.classList.add('hidden')
-}
 // Header scroll effect
 window.addEventListener('scroll', () => {
   let { top } = ctrl.getBoundingClientRect()
@@ -25,7 +20,11 @@ window.addEventListener('scroll', () => {
 document.addEventListener('DOMContentLoaded', () => {
   const progressBars = document.querySelectorAll('.progress')
   const progressContainer = document.getElementById('progresscontainer')
-
+  const loader = document.getElementById('loader')
+  if (loader) {
+    loader.classList.remove('fixed')
+    loader.classList.add('hidden')
+  }
   // Scroll observer
   const observer = new IntersectionObserver(
     (entries, obs) => {
